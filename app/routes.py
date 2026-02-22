74e5e8b219e47db0af566736b031218d96932683
Felipe Sampaio <felipesampaiocscj@gmail.com>
2026-02-22 16:32:29 -0300

    add convertion core and his endpoint /convert

--- app/routes.py
+++ app/routes.py
@@ -1,4 +1,7 @@
 from flask import Flask, render_template, request, session, redirect, url_for
+from git import Repo
+import docker
+
 from main import app
 
 @app.route('/') # rota da pagina inicial
@@ -14,4 +17,33 @@ def config():
         session['want_linux'] = bool(request.form.get('cLinux'))
         session['want_win'] = bool(request.form.get('cWindows'))
         session['want_alpine'] = bool(request.form.get('cAlpine'))
+
+        if session.get('want_linux') or session.get('want_win') or session.get('want_alpine'):
+            return redirect(url_for('convert'))
+
     return render_template("config.html")
+
+@app.route('/convert')
+def convert():
+    #tenta puxar o repositorio para previnir links errados
+    try:
+        clone_repo = Repo.clone_from(session.get('repo'), "./src")
+    except:
+        return redirect(url_for('config'))
+    
+    #PLATFORMS	win,linux,alpine
+    #resultado = ", ".join(lista)
+    env_platforms = [] # lista da env PLATAFORMS
+    if session['want_linux']:
+        env_platforms.append("linux")
+    if session['want_win']:
+        env_platforms.append("win")
+    if session['want_alpine']:
+        env_platforms.append("alpine")
+
+    single_env = ", ".join(env_platforms) # env PLATAFORMS em uma string pronta para bash
+
+    cliente = docker.from_env()
+    cliente.containers.run("fydeinc/pyinstaller", command="main.py", volumes={'/home/flp-sp/Projects/webpyinstaller/app/src' : {'bind' : '/src', 'mode' : 'rw'}}, environment=[f"PLATFORMS={single_env}"])
+
+    return render_template("convert.html")
