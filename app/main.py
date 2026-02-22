8860a1224e346484b2510747fee1eea6090d01ce
Felipe Sampaio <felipesampaiocscj@gmail.com>
2026-02-22 12:28:17 -0300

    add the configuration of convertion endpoint /config

--- app/main.py
+++ app/main.py
@@ -1,6 +1,7 @@
-from flask import Flask
+from flask import Flask, session
 
 app = Flask(__name__)
+app.secret_key = "1234" # talvez mudar
 
 from routes import *
 
--- app/routes.py
+++ app/routes.py
@@ -1,6 +1,17 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, request, session, redirect, url_for
 from main import app
 
 @app.route('/') # rota da pagina inicial
 def index():
     return render_template("index.html")
+
+@app.route('/config', methods=['GET','POST']) # rota da pagina de configurar o projeto para conversão
+def config():
+    if request.method == 'POST':
+        session['repo'] = request.form.get('repositorio') # link do repositorio
+
+        # armazena variaveis booleanas no session para as plataformas selecionadas a partir do checkbox
+        session['want_linux'] = bool(request.form.get('cLinux'))
+        session['want_win'] = bool(request.form.get('cWindows'))
+        session['want_alpine'] = bool(request.form.get('cAlpine'))
+    return render_template("config.html")
